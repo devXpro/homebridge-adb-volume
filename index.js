e4d9ba4ccc09b8c456e9297c9544519953a015ab
Roman Yarmolenko <devxpro@gmail.com>
2019-12-08 01:43:48 +0200

    bind this

--- index.js
+++ index.js
@@ -88,11 +88,10 @@ ReceiverVolume.prototype.setPowerOn = function(powerOn, callback) {
         this.setBrightness(this.volumeBeforeMute, callback);
     } else {
         this.log('Power Off');
-        this.log('Current volume: ' + this.volume + ' Volume before Mute: ' +this.volumeBeforeMute);
+        this.log('Current volume: ' + this.currentVolume + ' Volume before Mute: ' +this.volumeBeforeMute);
         this.volumeBeforeMute = this.currentVolume;
         this.setBrightness(0, callback);
     }
-    callback(null);
 }
 
 ReceiverVolume.prototype.getServices = function() {
