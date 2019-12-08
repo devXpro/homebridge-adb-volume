55b730ec28a18757a757d1e0667daa2ec57f2394
Roman Yarmolenko <devxpro@gmail.com>
2019-12-08 14:59:10 +0200

    callback shit

--- index.js
+++ index.js
@@ -162,8 +162,6 @@ ADBController.prototype.androidPower = function(powerOn, callback) {
                         callback(null);
                     }
                 }.bind(this));
-            } else{
-                callback(null);
             }
         }
     });
