8e8f5c38741a8000dc9775035a8ebdf439131d46
Roman Yarmolenko <devxpro@gmail.com>
2019-12-08 14:31:09 +0200

    Add control

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Service, Characteristic;
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory("homebridge-adb-volume", "adb-volume", ReceiverVolume);
+    homebridge.registerAccessory("homebridge-adb-volume", "adb-volume", ADBController);
 }
 
 function puts(error, stdout, stderr) {
@@ -12,15 +12,17 @@ function puts(error, stdout, stderr) {
     console.log(stderr);
 }
 
-function ReceiverVolume(log, config) {
+function ADBController(log, config) {
     this.log = log;
     this.currentVolume = 0;
     this.muted = false;
-    this.name = config['name'] || "Receiver Volume";
+    this.volumeDisable = config['volumeDisable'] || false;
+    this.volumeName = config['volumeName'] || "Receiver Volume";
     this.maxVolume = config['maxVolume'] || 15;
     this.host = config['host'];
     this.useFan = !!config['useFan']; // default to false, and make sure its a bool
     this.volumeBeforeMute = 3;
+    this.starters = config['starters'] || false;
     if (!this.host) {
         this.log.warn('Config is missing host/IP of receiver');
         callback(new Error('No host/IP defined.'));
@@ -30,7 +32,7 @@ function ReceiverVolume(log, config) {
     exec('adb connect '+ this.host, puts);
 }
 
-ReceiverVolume.prototype.getStatus = function(callback) {
+ADBController.prototype.getStatus = function(callback) {
     var command = "adb shell dumpsys audio | grep -A 4 STREAM_TTS |"+
         " grep speaker | cut -d')' -f2 | cut -d' ' -f2| cut -d',' -f1";
     exec(command, function(error, stdout, stderr) {
@@ -42,7 +44,7 @@ ReceiverVolume.prototype.getStatus = function(callback) {
     });
 }
 
-ReceiverVolume.prototype.setVolume = function(value, callback) {
+ADBController.prototype.setVolume = function(value, callback) {
     this.log('Current = '+ this.currentVolume + ' Setted = ' + value);
     if (value !== this.currentVolume) {
         exec('adb shell service call audio 7 i32 3 i32 ' + value + ' i32 1', function(error, stdout, stderr) {
@@ -60,13 +62,13 @@ ReceiverVolume.prototype.setVolume = function(value, callback) {
     }
 };
 
-ReceiverVolume.prototype.setBrightness = function(newLevel, callback) {
+ADBController.prototype.setBrightness = function(newLevel, callback) {
     var onePercent = this.maxVolume / 100;
     var relativeVolume = Math.round(onePercent * newLevel);
     this.setVolume(relativeVolume, callback);
 }
 
-ReceiverVolume.prototype.getBrightness = function(callback) {
+ADBController.prototype.getBrightness = function(callback) {
     this.getStatus(function(status) {
         if (status) {
             var oneStep = 100 / this.maxVolume;
@@ -79,14 +81,14 @@ ReceiverVolume.prototype.getBrightness = function(callback) {
     }.bind(this));
 };
 
-ReceiverVolume.prototype.getPowerOn = function(callback) {
+ADBController.prototype.getPowerOn = function(callback) {
     this.getStatus(function(status) {
         var powerState = status ? 1 : 0;
         callback(null, powerState);
     }.bind(this));
 }
 
-ReceiverVolume.prototype.setPowerOn = function(powerOn, callback) {
+ADBController.prototype.setPowerOn = function(powerOn, callback) {
     if (powerOn) {
         if (this.muted) {
             this.log('Power On');
@@ -104,24 +106,100 @@ ReceiverVolume.prototype.setPowerOn = function(powerOn, callback) {
     }
 };
 
-ReceiverVolume.prototype.getServices = function() {
-    var lightbulbService = this.useFan ? new Service.Fan(this.name) : new Service.Lightbulb(this.name);
+ADBController.prototype.setPowerState = function(targetService, powerState, callback, context) {
+    var funcContext = 'fromSetPowerState';
 
-    lightbulbService
-        .getCharacteristic(Characteristic.On)
-        .on('get', this.getPowerOn.bind(this))
-        .on('set', this.setPowerOn.bind(this));
-    if (this.useFan) {
-        lightbulbService
-            .addCharacteristic(new Characteristic.RotationSpeed())
-            .on('get', this.getBrightness.bind(this))
-            .on('set', this.setBrightness.bind(this));
+    // Callback safety
+    if (context == funcContext) {
+        if (callback) {
+            callback();
+        }
+
+        return;
     }
-    else {
-        lightbulbService
-            .addCharacteristic(new Characteristic.Brightness())
-            .on('get', this.getBrightness.bind(this))
-            .on('set', this.setBrightness.bind(this));
-    }
-    return [lightbulbService];
+    this.services.forEach(function (switchService, idx) {
+        if (idx === 0 && !this.volumeDisable) {
+            // Don't check the VolumeService which is at idx=0
+            return;
+        }
+
+        if (targetService.subtype === switchService.subtype) {
+            if(powerState){
+                this.androidPower(true, callback);
+                if (this.starters[idx]['command']) {
+                    var commands = this.starters[idx]['command'].split(' ');
+                    commands.forEach(function(keycode){
+                        exec('adb shell input keyevent ' + keycode, function(error, stdout, stderr) {
+                            callback(null);
+                        });
+                    });
+                }
+            } else{
+                this.androidPower(false, callback);
+            }
+        } else {
+            switchService.getCharacteristic(Characteristic.On).setValue(false, undefined, funcContext);
+        }
+    }.bind(this));
+    callback(null);
 }
+
+ADBController.prototype.androidPower = function(powerOn, callback) {
+    var command = "adb shell dumpsys power | grep 'mHoldingDisplaySuspendBlocker'";
+    exec(command, function(error, stdout, stderr) {
+        if (stderr) {
+            callback(null);
+        } else {
+            var searchElement = powerOn ? "false" : "true";
+            if(stdout.indexOf(searchElement) !== -1){
+                exec('adb shell input keyevent 26', function(error, stdout, stderr) {
+                    if (stderr) {
+                        this.log('Error = ' + stderr);
+                        callback(error);
+                    } else {
+                        callback(null);
+                    }
+                }.bind(this));
+                callback(null);
+            } else{
+                callback(null);
+            }
+        }
+    });
+};
+
+ADBController.prototype.getServices = function() {
+   this.services = [];
+   if(!this.volumeDisable) {
+       var lightbulbService = this.useFan ? new Service.Fan(this.name) : new Service.Lightbulb(this.name);
+
+       lightbulbService.getCharacteristic(Characteristic.On).
+           on('get', this.getPowerOn.bind(this)).
+           on('set', this.setPowerOn.bind(this));
+       if (this.useFan) {
+           lightbulbService.addCharacteristic(new Characteristic.RotationSpeed()).
+               on('get', this.getBrightness.bind(this)).
+               on('set', this.setBrightness.bind(this));
+       } else {
+           lightbulbService.addCharacteristic(new Characteristic.Brightness()).
+               on('get', this.getBrightness.bind(this)).
+               on('set', this.setBrightness.bind(this));
+       }
+       this.services.push(lightbulbService);
+   }
+    if (this.starters) {
+        this.starters.forEach(function(starter) {
+            var switchService = new Service.Switch(starter.name, starter.name);
+            switchService
+                .getCharacteristic(Characteristic.On)
+                .on('set', this.setPowerState.bind(this, switchService));
+
+            this.services.push(switchService);
+        }.bind(this));
+    }
+    return this.services;
+}
+
+
+
+
